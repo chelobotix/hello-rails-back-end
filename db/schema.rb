2bb9aa1d8c406d1492c10ae0dc656554501e044e
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-06-14 09:27:41 -0400

    No linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,12 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_13_195929) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "messages", force: :cascade do |t|
-    t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'messages', force: :cascade do |t|
+    t.string 'text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
 end
